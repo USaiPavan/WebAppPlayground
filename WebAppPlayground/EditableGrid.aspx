b8af447367be8ca84a113d3920caed6dd11516fe
Sai Pavan Viswanath Upadhyayula <usaipavan@outlook.com>
2012-11-17 23:33:10 +0530

    Editable Grid Implementation
    Implemented editable grid view in the file EditableGrid.aspx for the
    blog post

--- WebAppPlayground/EditableGrid.aspx
+++ WebAppPlayground/EditableGrid.aspx
@@ -1,25 +1,55 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditableGrid.aspx.cs" Inherits="WebApplication1.EditableGrid" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditableGrid.aspx.cs" Inherits="WebApplication1.EditableGrid" MaintainScrollPositionOnPostback="true" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Editable Grid</title>
+    <script type="text/javascript">
+        function updateEditStatus(clientID) {
+            var hiddenEditControl = document.getElementById(clientID);
+            if (hiddenEditControl != null) {
+                hiddenEditControl.value = "true";
+            }
+        }
+    </script>
+    <style type="text/css">
+    .hiddencol
+    {
+        display: none;
+    }
+    </style>
 </head>
 <body>
     <form id="frmEditableGrid" runat="server">
     <div>
-        
-        <asp:GridView ID="grdEditableGrid" runat="server">
-        <Columns>
-            <asp:TemplateField>
-            <ItemTemplate>
-                <asp:TextBox runat="server" ID="txtCategoryID" Text='<% DataBinder.Eval("CategoryID") %>' />
-            </ItemTemplate>
-            </asp:TemplateField>
-        </Columns>
+        <asp:GridView ID="grdEditableGrid" runat="server" AutoGenerateColumns="false" OnRowDataBound="grdEditableGrid_RowDataBound">
+            <Columns>
+                <asp:TemplateField HeaderText="CategoryID" ItemStyle-CssClass="hiddencol" HeaderStyle-CssClass="hiddencol" >
+                    <ItemTemplate>
+                        <asp:Label runat="server" ID="lblCategoryID" Text='<%# DataBinder.Eval(Container.DataItem, "CategoryID") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="CategoryName">
+                    <ItemTemplate>
+                        <asp:TextBox runat="server" ID="txtCategoryName" Text='<%# DataBinder.Eval(Container.DataItem, "CategoryName") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Description">
+                    <ItemTemplate>
+                        <asp:TextBox runat="server" ID="txtDescription" Text='<%# DataBinder.Eval(Container.DataItem, "Description") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Edit Status" ItemStyle-CssClass=hiddencol HeaderStyle-CssClass=hiddencol>
+                    <ItemTemplate>
+                        <asp:HiddenField runat="server" ID="hdnEditStatus" Value='' EnableViewState="true" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
         </asp:GridView>
-            
+    </div>
+    <div>
+        <asp:Button Text="Save Data" runat="server" ID="btnSaveData" 
+            onclick="btnSaveData_Click" />
     </div>
     </form>
 </body>
--- WebAppPlayground/EditableGrid.aspx.cs
+++ WebAppPlayground/EditableGrid.aspx.cs
@@ -11,14 +11,21 @@ using System.Data;
 namespace WebApplication1
 {
     public partial class EditableGrid : System.Web.UI.Page
-    {
+    {   
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            DataTable dtCategories = new DataTable(); 
-            getCategories(out dtCategories);
+            if (!Page.IsPostBack)
+            {
+                DataTable dtCategories = new DataTable();
+                getCategories(out dtCategories);
+                dtCategories.Columns["CategoryID"].ReadOnly = true;
 
-            grdEditableGrid.DataSource = dtCategories;
-            grdEditableGrid.DataBind();
+
+                grdEditableGrid.DataSource = dtCategories;
+                grdEditableGrid.DataBind();
+            }
+            
         }
 
         private DataTable getCategories(out DataTable dtCategories)
@@ -33,9 +40,11 @@ namespace WebApplication1
                     cmdCategories.CommandText = "SELECT * FROM CATEGORIES";
                     cmdCategories.CommandType = System.Data.CommandType.Text;
 
-                    nwndConnection.Open();                    
+                    nwndConnection.Open();
                     dtCategories.Load(cmdCategories.ExecuteReader());
                     nwndConnection.Close();
+
+                    dtCategories.Columns["CategoryID"].ReadOnly = true;
                 }
             }
             catch (Exception ex)
@@ -44,6 +53,52 @@ namespace WebApplication1
             }
 
             return dtCategories;
+        }
+
+        private DataTable getDirtyRows()
+        {
+            DataTable dtDirtyRows = new DataTable();            
+
+            dtDirtyRows.Columns.Add("CategoryID");
+
+            foreach (GridViewRow item in grdEditableGrid.Rows)
+            {
+                HiddenField editStatus = item.FindControl("hdnEditStatus") as HiddenField;
+                if (editStatus != null)
+                {
+                    if (editStatus.Value == "true")
+                    {
+                        var lblCategoryID = item.FindControl("lblCategoryID") as Label;
+                        if (lblCategoryID != null)
+                        {
+                            DataRow dr = dtDirtyRows.Rows.Add();
+                            dr["CategoryID"] = lblCategoryID.Text;
+                        }
+                    }
+                }
+            }
+
+            return dtDirtyRows;
+        }
+
+        protected void grdEditableGrid_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            TextBox categoryName = e.Row.FindControl("txtCategoryName") as TextBox;
+            if (categoryName != null)
+            {
+                categoryName.Attributes.Add("onkeyup", "updateEditStatus(\"" + e.Row.FindControl("hdnEditStatus").ClientID + "\");");
+            }           
+
+            TextBox description = e.Row.FindControl("txtDescription") as TextBox;
+            if (description != null)
+            {
+                description.Attributes.Add("onkeyup", "updateEditStatus(\"" + e.Row.FindControl("hdnEditStatus").ClientID + "\");");
+            }
+        }
+
+        protected void btnSaveData_Click(object sender, EventArgs e)
+        {
+            getDirtyRows();
         }
     }
 }
